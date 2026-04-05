638cae425773ff53ccbfe7738918c14a032a7aae
1121529 <s1121529@mail.yzu.edu.tw>
2026-04-06 02:12:11 +0800

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -3,7 +3,7 @@ import json
 import csv
 
 
-url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-A0021-001?Authorization=rdec-key-123-45678-011121314"
+url = "https://stats.moe.gov.tw/files/opendata/106_25.json"
 
 def main():
     try:
